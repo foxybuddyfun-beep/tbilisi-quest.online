bde6352338284d4981cec45ae5faf4ccfb0605b3
foxybuddyfun-beep <foxybuddyfun@gmail.com>
2025-12-21 17:07:24 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 // config.js
-export const API_URL = "https://script.google.com/macros/s/AKfycbwIP9eqpKM43N_YCg4i99M49ti1fATYA9W0Syr0Dwd5WBqy4AhEASKspv18Fgh5I5QH/exec"; 
+export const API_URL = "https://script.google.com/macros/s/AKfycbyQYyhFtGfgq5Yu_isoO1uNtssZdkD4uUIb4f1BlqkFq1i5nGCq8a4EYBA_f_4FV0Y/exec"; 
+
+
 
 
 
